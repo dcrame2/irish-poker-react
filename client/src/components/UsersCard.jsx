069e205c9f9a97f17a7ebab492dfa95280be2f70
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2023-02-06 19:23:18 -0600

    update attempts

--- client/src/components/UsersCard.jsx
+++ client/src/components/UsersCard.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-const UsersCardContainer = styled.div `
-         background-color: lightblue;
-      padding: 10px;
-
+const UsersCardContainer = styled.div`
+  background-color: lightblue;
+  padding: 10px;
 `;
 
-const UsersCard = ({users}) => {
+const UsersCard = ({ users, playerNumber }) => {
+  console.log(playerNumber);
   return (
-    <UsersCardContainer>{users}</UsersCardContainer>
-  )
-}
+    <UsersCardContainer>
+      Player #{playerNumber}: {users}
+    </UsersCardContainer>
+  );
+};
 
-export default UsersCard
+export default UsersCard;
