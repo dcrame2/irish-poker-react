bf71aada6b12ac85799ed07eecc06525a44646fd
Dylan Cramer <dcrame2@gmail.com>
2023-02-07 09:15:28 -0600

    commiting

--- client/src/pages/GameScreen.jsx
+++ client/src/pages/GameScreen.jsx
@@ -8,7 +8,7 @@ const GameContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: green;
+  background-color: grey;
   .inner-container {
     display: flex;
     flex-direction: column;
